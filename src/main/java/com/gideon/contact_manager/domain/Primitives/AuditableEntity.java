ff3cfc02c3f470a5b0793a7191c762ac10048b4b
Gideon Isa <innocentgideon10@gmail.com>
2025-02-09 10:34:43 +0100

    connected to postgres db

--- src/main/java/com/gideon/contact_manager/domain/Primitives/AuditableEntity.java
+++ src/main/java/com/gideon/contact_manager/domain/Primitives/AuditableEntity.java
@@ -1,25 +1,27 @@
 package com.gideon.contact_manager.domain.Primitives;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AuditableEntity {
-    @CreatedDate
+    @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_on", nullable = false, updatable = false)
     private Date createdOn;
 
+    @CreatedBy
     @Column(name = "created_by", nullable = false, updatable = false)
     private String createdBy;
 
